8551ac71980a0eac56484dee60f92a31edc1106d
Ryan Catalfu <rpcatalf@ncsu.edu>
2021-10-06 20:08:56 -0400

    Starter for landing page.

--- frontend/src/app/landing-page/landing-page.component.ts
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: [
+    './bootstrap.min.css',
+    './landing-page.component.css'
+  ]
 })
 export class LandingPageComponent implements OnInit {
 
