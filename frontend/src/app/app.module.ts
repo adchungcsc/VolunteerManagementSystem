6195799487e52db2402b5338013d0f656033e521
Ryan Catalfu <rpcatalf@ncsu.edu>
2021-10-08 12:22:31 -0400

    Worked on the Event card - not finished though

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EventCardComponent } from './event-card/event-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    EventCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/event-card/event-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  styleUrls: ['./event-card.component.css']
+})
+export class EventCardComponent implements OnInit {
+
+  // TODO NEED TO INJECT and work on this.
+  /**
+   * 
+   * @param data 
+   */
+  constructor(public data: any) { 
+
+   }
+
+  ngOnInit(): void {
+  }
+
+}
