9e97cfdf14eb26c86670896761f763139ae18b10
Alex Chung <alexchung@pop-os.localdomain>
2021-10-15 18:16:25 -0400

    hotfix missing , + dup dep

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,8 +30,6 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatSelectModule} from "@angular/material/select";
 import {MatRadioModule} from "@angular/material/radio";
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
 import {MatDatepickerModule} from "@angular/material/datepicker";
 import {MatNativeDateModule} from "@angular/material/core";
 import {CreateEventPageComponent} from "./create-event-page/create-event-page.component";
@@ -65,7 +63,7 @@ import {CreateEventPageComponent} from "./create-event-page/create-event-page.co
     MatRadioModule,
     MatCardModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
 
     MatButtonModule,
     MatPaginatorModule,
