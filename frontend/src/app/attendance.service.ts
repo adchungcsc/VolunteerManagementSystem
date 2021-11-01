a1bf809e4a9c29e3a00dd4bd15dfc25a1f3e5248
Ryan Catalfu <rpcatalf@ncsu.edu>
2021-11-01 11:57:14 -0400

    Started to structure the attendance dialog
    And added the service for the API.

---
+++ frontend/src/app/attendance.service.ts
@@ -0,0 +1,137 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttendanceService {
+
+  /** Where is the app running? */
+	private apiBaseUrl = environment.host;
+
+	/** Attendance Route */
+	private attendanceRoute = this.apiBaseUrl + '/api/v1/attend/';
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Headers used for ease
+   */
+  httpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
+
+  /** Http Options - pulled from prior sample code */
+	httpOptions = {
+		headers: this.httpHeaders
+	}
+
+  /** Error Handler - pulled from sample code */
+	private handleAnyErrors(error: HttpErrorResponse): any {
+		console.error(`Error from server with code ${error.status}, ` 
+      + `${error.error['error']?.message}`);
+		return throwError('Error: ' + `${error.error['error']?.message}`);
+	}
+
+
+
+  /**
+   * Gets the attendance for the specific user and event provided.
+   * @param eventId The ID of the event to get attendance.
+   * @param userId The user ID for the user to get their attendance for the event, or empty if current user.
+   */
+  getSpecificUserAttendanceForEvent(eventId: number, userId?: number): Observable<any> {
+
+    // If not a valid ID/if null, error.
+    console.log("the ID is " + eventId);
+    if ((eventId == undefined || eventId == null) && eventId !== 0) {
+      throw new Error('Invalid Event ID');
+    }
+
+    // If not provided, get for current user.
+    // TODO ON BACKEND THE API SHOULD ONLY ALLOW USER TO GET THEIR OWN IF NOT ADMIN.
+    if ((userId == undefined || userId == null) && userId !== 0) {
+      // then assign it to be the current user.
+      userId = /* TODO ADD REAL USER ID HERE */ 1;
+    }
+    
+    // Returns the result or the error....
+    let results = this.http.get(this.attendanceRoute + 'event/' + eventId + '/' + userId, this.httpOptions).pipe(
+      catchError(this.handleAnyErrors)
+    );
+
+    // TODO REMOVE
+    console.log(results);
+
+    return results;
+  }
+
+
+  /**
+   * Gets attendance for the event ID provided.
+   * @param eventId The ID of the event to get
+   */
+  getAllAttendanceForEvent(eventId: number): Observable<any> {
+    // If not a valid ID/if null, error.
+    console.log("the ID is " + eventId);
+    if ((eventId == undefined || eventId == null) && eventId !== 0) {
+      throw new Error('Invalid Event ID');
+    }
+
+    // Returns the result or the error....
+    let results = this.http.get(this.attendanceRoute + 'event/' + eventId, this.httpOptions).pipe(
+      catchError(this.handleAnyErrors)
+    );
+
+    // TODO REMOVE
+    console.log(results);
+
+    return results;
+  }
+
+
+  // TODO NEED TO ACCOUNT FOR POTENTIALLY NOT NUMBER FOR USER ID.
+  // ALSO, WE MIGHT NOT WANT TO GET THE IMAGES - EITHER A LINK or a set of links to make other calls is fine.
+  /**
+   * Gets all attendance for events.
+   * @param userId The User ID to use when getting attendance logs.
+   */
+  getAllAttendanceForUser(userId: number): Observable<any> {
+    // If not a valid ID/if null, error.
+    console.log("the ID is " + userId);
+    if ((userId == undefined || userId == null) && userId !== 0) {
+      throw new Error('Invalid Event ID');
+    }
+
+    // Returns the result or the error....
+    let results = this.http.get(this.attendanceRoute + 'user/' + userId, this.httpOptions).pipe(
+      catchError(this.handleAnyErrors)
+    );
+
+    // TODO REMOVE
+    console.log(results);
+
+    return results;
+  }
+
+  /**
+   * Enrolls the current volunteer into the event.
+   * TODO SHOULD WE ALSO PROVIDE THE USER ID?
+   * @param eventId The Id of the event.
+   * @param formData The Contents of the form (image, description).
+   */
+   submitAttendanceForEvent(eventId: number, formData: FormData): Observable<any> {
+    if ((eventId == undefined || eventId == null) && eventId !== 0) {
+      throw new Error('Invalid ID');
+    }
+    return this.http.post(this.attendanceRoute, {'eventId': eventId, 'formData': formData}, this.httpOptions).pipe(
+      catchError(this.handleAnyErrors)
+    );
+  }
+
+}
+
--- frontend/src/app/proof-dialog/proof-dialog.component.ts
+++ frontend/src/app/proof-dialog/proof-dialog.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-proof-dialog',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProofDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ProofDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: {eventId: number, eventName: string}) { }
 
   ngOnInit(): void {
   }
