7c3009254594b0cc9683a46be9571c24ffe8d0b6
Ryan Catalfu <rpcatalf@ncsu.edu>
2021-11-18 10:57:11 -0500

    Started on the Create Event fixes. Not complete.

--- frontend/src/app/create-event-page/create-event-page.component.ts
+++ frontend/src/app/create-event-page/create-event-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-event-page',
@@ -7,9 +8,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEventPageComponent implements OnInit {
 
-  constructor() { }
+  /** The form object for fields to be edited */
+  form = new FormGroup({
+    event_name: new FormControl('', [Validators.minLength(2), Validators.required]),
+    start_date: new FormControl(new Date(), Validators.required),
+    end_date: new FormControl(new Date(), Validators.required),
+    start_time: new FormControl(new Date()),
+    end_time: new FormControl(new Date()),
+    num_of_volunteers: new FormControl(10, Validators.required),
+    waitlist_num: new FormControl(0),
+    address: new FormControl('', [Validators.minLength(2), Validators.required]),
+    event_organizer: new FormControl(1),
+    description: new FormControl(''),
+    image: new FormControl('')
+  });
+
+  // A String representing if this is creating an event.
+  action: string = 'Create';
+
+  constructor() { 
+    
+   }
+
+  // /**
+  //  * Creates the form as new again.
+  //  */
+  // configureNewForm(): void {
+  //   this.form = new FormGroup({
+  //     event_name: new FormControl('', [Validators.minLength(2), Validators.required]),
+  //     start_date: new FormControl(new Date(), Validators.required),
+  //     end_date: new FormControl(new Date(), Validators.required),
+  //     start_time: new FormControl(new Date()),
+  //     end_time: new FormControl(new Date()),
+  //     num_of_volunteers: new FormControl(10, Validators.required),
+  //     waitlist_num: new FormControl(0),
+  //     address: new FormControl('', [Validators.minLength(2), Validators.required]),
+  //     event_organizer: new FormControl(1),
+  //     description: new FormControl(''),
+  //     image: new FormControl('')
+  //   });
+  // }
 
   ngOnInit(): void {
+  }
+
+  // When submitting
+  onSubmit() {
+    // First, bundle the form
+
+    // Second, send the form and the image to a service to send to the server
+
+    // Third, wait for a response
+
+    // Fourth, display a "snackbar"
+
+    // Fifth, clear the form on the page
+
+  }
+
+  // Clears the form
+  onClear() {
+    this.form.reset({
+      event_name: '',
+      start_date: new Date(),
+      end_date: new Date(),
+      start_time: new Date(),
+      end_time: new Date(),
+      num_of_volunteers: 10,
+      waitlist_num: 0,
+      address: '',
+      event_organizer: 1,
+      description: '',
+      image: ''
+    });
   }
 
 }
