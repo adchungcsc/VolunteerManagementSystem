908a3b99c3f3f385d32e86b36a2b5bf59d040880
adchung <adchung@ncsu.edu>
2021-11-19 03:13:37 -0500

    Update attendance.model.js

--- server/orm/models/attendance.model.js
+++ server/orm/models/attendance.model.js
@@ -33,5 +33,5 @@ module.exports = (sequelize) => {
             timestamps: false,
             freezeTableName: true,
             tableName: "event_attendance"
-        }).schema("participance_updated_cols");
+        }).schema("public");
 }
