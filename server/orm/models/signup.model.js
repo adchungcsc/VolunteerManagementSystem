dd2d30de8bd43b354fb6a1a0192f332ae89264a2
adchung <adchung@ncsu.edu>
2021-11-19 03:14:00 -0500

    Update signup.model.js

--- server/orm/models/signup.model.js
+++ server/orm/models/signup.model.js
@@ -25,5 +25,5 @@ module.exports = (sequelize) => {
             timestamps: false,
             freezeTableName: true,
             tableName: "event_signup"
-        }).schema("participance_updated_cols");
+        }).schema("public");
 }
