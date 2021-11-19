7567017621030b9c8159faf4112d66df7b7487ba
Alex Chung <alexchung@pop-os.localdomain>
2021-11-19 08:03:41 -0500

    Get user id

--- frontend/src/app/users.service.ts
+++ frontend/src/app/users.service.ts
@@ -1,20 +1,21 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { observable, Observable, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {observable, Observable, throwError} from 'rxjs';
+import {environment} from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-    /** Where is the app running? */
-	private apiBaseUrl = environment.host;
+  /** Where is the app running? */
+  private apiBaseUrl = environment.host;
 
-	/** User Route */
-	private usersRoute = this.apiBaseUrl + '/api/v1/user/';
+  /** User Route */
+  private usersRoute = this.apiBaseUrl + '/api/v1/user/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   /**
    * Headers used for ease
@@ -25,19 +26,24 @@ export class UsersService {
   });
 
   /** Http Options - pulled from prior sample code */
-	httpOptions = {
-		headers: this.httpHeaders
-	}
+  httpOptions = {
+    headers: this.httpHeaders
+  }
 
   /** Error Handler - pulled from sample code */
-	private handleAnyErrors(error: HttpErrorResponse): any {
-		console.error(`Error from server with code ${error.status}, ` 
+  private handleAnyErrors(error: HttpErrorResponse): any {
+    console.error(`Error from server with code ${error.status}, `
       + `${error.error['error']?.message}`);
-		return throwError('Error: ' + `${error.error['error']?.message}`);
-	}
+    return throwError('Error: ' + `${error.error['error']?.message}`);
+  }
 
   getCurrentUserId(): number {
-    // TODO WRITE THIS
+    fetch('/api/v1/user')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        return data[0].user_id
+      })
     return 1;
   }
 
