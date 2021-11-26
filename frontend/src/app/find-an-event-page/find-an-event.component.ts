e54c037cf90292560ea88872bad2efa730e669e0
Ryan Catalfu <rpcatalf@ncsu.edu>
2021-11-26 15:45:56 -0500

    Added basic pagination to the Find Events frontend

--- frontend/src/app/find-an-event-page/find-an-event.component.ts
+++ frontend/src/app/find-an-event-page/find-an-event.component.ts
@@ -4,6 +4,7 @@ import { EventsService } from '../events.service';
 import { EventItem, EventsDataSource } from './events-datasource';
 import { CollectionViewer } from "@angular/cdk/collections";
 import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-find-an-event',
@@ -18,6 +19,15 @@ export class FindAnEventComponent implements OnInit {
 
   viewChange: any;
 
+  currentPageSize: number = 10;
+
+  currentSkipToken: number = 0;
+
+  currentPageIndex: number = 1;
+
+  // The total number of events.
+  numberOfEvents: number = 100;
+
   // This is for the toggle on if old events should be displayed.
   displayOldEvents: boolean = false;
 
@@ -28,11 +38,16 @@ export class FindAnEventComponent implements OnInit {
   ngOnInit(): void {
     this.dataSource = new EventsDataSource(this.eventsService);
 
+    // Sets page size to default
+    this.currentPageSize = 10;
+    this.currentSkipToken = 0;
+    this.currentPageIndex = 1;
+
     // CollectionViewer a = new CollectionViewer();
 
     this.dataSourceInfo = this.dataSource.connect(this);
     // TODO increase page size.
-    this.dataSource.loadEvents(this.displayOldEvents, '', 0, 10);
+    this.dataSource.loadEvents(this.displayOldEvents, '', this.currentSkipToken, this.currentPageSize);
 
     console.log('view change');
     console.log(this.viewChange);
@@ -45,7 +60,9 @@ export class FindAnEventComponent implements OnInit {
       debounceTime(300),
       distinctUntilChanged(),
       tap(() => {
-        this.dataSource.loadEvents(this.displayOldEvents, this.input.nativeElement.value, 0, 10);
+        this.currentSkipToken = 0;
+        this.currentPageIndex = 1;
+        this.dataSource.loadEvents(this.displayOldEvents, this.input.nativeElement.value, this.currentSkipToken, this.currentPageSize);
       })
     ).subscribe();
   }
@@ -55,7 +72,9 @@ export class FindAnEventComponent implements OnInit {
    * @param event The event
    */
   displayPreviousEvents() {
-    this.dataSource.loadEvents(this.displayOldEvents, this.input.nativeElement.value, 0, 10);
+    this.currentPageIndex = 1;
+    this.currentSkipToken = 0;
+    this.dataSource.loadEvents(this.displayOldEvents, this.input.nativeElement.value, this.currentSkipToken, this.currentPageSize);
   }
 
   /**
@@ -68,6 +87,27 @@ export class FindAnEventComponent implements OnInit {
     // Actually filter it.
     
 
+  }
+
+  /**
+   * Changes the displayed data.
+   * @param event The page change
+   */
+  onPageChange(event: PageEvent) {
+    console.log('Paginator:');
+    console.log(event);
+    console.log('Current index ' + this.currentPageIndex);
+    console.log('Current page size ' + this.currentPageSize);
+    console.log('Current Skip Token ' + this.currentSkipToken);
+    console.log('Event page size is ' + event.pageSize + ' and index is ' + event.pageIndex);
+    
+
+    // Adjust page size and skip token due to changes made.
+    this.currentPageSize = event.pageSize;
+    this.currentSkipToken = (event.pageIndex - 1) * event.pageSize;
+
+
+    this.dataSource.loadEvents(this.displayOldEvents, this.input.nativeElement.value, this.currentSkipToken, this.currentPageSize);
   }
 
 }
