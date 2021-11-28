c545a4814768c564c6ab8b870cb7531ad646cf22
Alex Chung <alexchung@pop-os.localdomain>
2021-11-28 15:56:17 -0500

    Misc. changed to API + fixed events api endpoint

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  host: 'http://localhost:4200',
+  host: 'http://participance.eastus.cloudapp.azure.com',
   SOCKET_ENDPOINT: 'http://localhost:3000'
 };
